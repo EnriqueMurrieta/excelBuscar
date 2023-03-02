52b59dc791fb6b1e49b7627fcb9cc458378cd2ae
Enrique <enrique_murrieta@hotmail.com>
2023-03-02 11:36:43 -0800

    eeeee eeeee

--- src/registroPaciente/RegistroPaciente.js
+++ src/registroPaciente/RegistroPaciente.js
@@ -62,7 +62,7 @@ function RegistroPaciente() {
 					<button type="submit" />
 				</form>*/}
 
-				<form name='testing10' data-netlify="true" >
+				<form name='testing10' data-netlify="true" method='post' >
 					<input type="hidden" name="form-name-thingy" value="testing10" />
 					<input required type="text" name="name" />
 					{/*<input required type="email" name="email" />
