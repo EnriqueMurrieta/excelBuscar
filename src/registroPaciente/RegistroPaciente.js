7142dd5e061ad77a3c3ec63e7b90ffbc4b5ea8fa
Enrique <enrique_murrieta@hotmail.com>
2023-03-01 21:01:42 -0800

    RegistroPaciente POST without axios

--- src/registroPaciente/RegistroPaciente.js
+++ src/registroPaciente/RegistroPaciente.js
@@ -9,171 +9,176 @@ function RegistroPaciente() {
 				<h2 className='titulo'>
 					Registrar paciente
 				</h2>
-				<div className='fila'>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Nombre
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
-						</div>
-						<input className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Ap. Paterno
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
-						</div>
-						<input className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Ap. Materno
-							</p>
-						</div>
-						<input className='input' />
-					</div>
-				</div>
 
-				<div className='fila'>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Fecha Nac.
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
-						</div>
-						<input type="date" className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Genero
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
-						</div>
-						<select className='input'>
-							<option value="">-</option>
-							<option value="masculino">Masculino</option>
-							<option value="femenino">Femenino</option>
-						</select>
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Estado Civil
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
-						</div>
-						<select className='input'>
-							<option value="">-</option>
-							<option value="masculino">Soltero</option>
-							<option value="femenino">Casado</option>
-						</select>
-					</div>
-				</div>
+				<form name="RegistroPacience" data-netlify='true'>
 
-				<div className='fila'>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Telefono Principal
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
+					<div className='fila'>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Nombre
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="text" name="nombre" className='input' />
 						</div>
-						<input className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Tipo Telefono
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Ap. Paterno
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="text" name="paterno" className='input' />
 						</div>
-						<select className='input'>
-							<option value="">-</option>
-							<option value="masculino">Móvil</option>
-							<option value="femenino">Fijo</option>
-						</select>
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Correo Principal
-							</p>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Ap. Materno
+								</p>
+							</div>
+							<input type="text" name="materno" className='input' />
 						</div>
-						<input type='email' className='input' />
 					</div>
-				</div>
 
-				<div className='fila'>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Telefono Emergencia
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
+					<div className='fila'>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Fecha Nac.
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="date" name="nacimiento" className='input' />
 						</div>
-						<input className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Contacto Emergencia
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Genero
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<select name="genero" className='input'>
+								<option value="">-</option>
+								<option value="masculino">Masculino</option>
+								<option value="femenino">Femenino</option>
+							</select>
 						</div>
-						<input className='input' />
-					</div>
-					<div className='campo'>
-						<div className='titulo'>
-							<p style={{ marginBottom: "0%" }}>
-								Seguro Gastos Medicos
-							</p>
-							<p style={{ color: 'red', marginBottom: "0%" }}>
-								*
-							</p>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Estado Civil
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<select name="estadoCivil" className='input'>
+								<option value="">-</option>
+								<option value="masculino">Soltero</option>
+								<option value="femenino">Casado</option>
+							</select>
 						</div>
-						<select className='input' multiple={true}>
-							<option value="">-</option>
-							<option value="uno">Opción 1</option>
-							<option value="dos">Opción 2</option>
-							<option value="tres">Opción 3</option>
-							<option value="cuatro">Opción 4</option>
-						</select>
 					</div>
-				</div >
 
-				<div className='fila'>
-					<div className='campo' style={{height:"100%"}}>
-						<button className='guardar' disabled='true' >Guardar</button>
+					<div className='fila'>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Telefono Principal
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="tel" name="telefono" className='input' />
+						</div>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Tipo Telefono
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<select type="text" name="tipoTelefono" className='input'>
+								<option value="">-</option>
+								<option value="masculino">Móvil</option>
+								<option value="femenino">Fijo</option>
+							</select>
+						</div>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Correo Principal
+								</p>
+							</div>
+							<input type='email' name="correo" className='input' />
+						</div>
 					</div>
-				</div>
+
+					<div className='fila'>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Telefono Emergencia
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="tel" name="emergenciaTelefono" className='input' />
+						</div>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Contacto Emergencia
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<input type="text" name="emergenciaContacto" className='input' />
+						</div>
+						<div className='campo'>
+							<div className='titulo'>
+								<p style={{ marginBottom: "0%" }}>
+									Seguro Gastos Medicos
+								</p>
+								<p style={{ color: 'red', marginBottom: "0%" }}>
+									*
+								</p>
+							</div>
+							<select type="text" name="seguro" className='input' multiple={true}>
+								<option value="">-</option>
+								<option value="uno">Opción 1</option>
+								<option value="dos">Opción 2</option>
+								<option value="tres">Opción 3</option>
+								<option value="cuatro">Opción 4</option>
+							</select>
+						</div>
+					</div >
+
+					<div className='fila'>
+						<div className='campo' style={{ height: "100%" }}>
+							<button type='submit' className='guardar' >Guardar</button>
+						</div>
+					</div>
+
+				</form>
 
 			</div>
 			<div className="coincidencias">
-				<Coincidencias/>
+				<Coincidencias />
 			</div>
 		</div>
 	);
