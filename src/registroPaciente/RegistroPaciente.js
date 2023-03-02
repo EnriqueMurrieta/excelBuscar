617606c556df304f60f7f871147b35bd49e67326
Enrique <enrique_murrieta@hotmail.com>
2023-03-02 11:13:58 -0800

    eeeee eeeee

--- src/registroPaciente/RegistroPaciente.js
+++ src/registroPaciente/RegistroPaciente.js
@@ -62,7 +62,7 @@ function RegistroPaciente() {
 					<button type="submit" />
 				</form>*/}
 
-				<form name='testing6' action='/testing6' method="POST">
+				<form name='testing6' action='/testing6' >
 					<input required type="text" name="name" />
 					{/*<input required type="email" name="email" />
 					<textarea required name="message" ></textarea>
