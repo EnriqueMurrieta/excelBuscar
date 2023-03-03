321823040a985450a628bbd7b96bf3c841e99a46
Enrique <enrique_murrieta@hotmail.com>
2023-03-02 18:51:01 -0800

    sss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import RegistroPaciente from './registroPaciente/RegistroPaciente';
+//import RegistroPaciente from './registroPaciente/RegistroPaciente';
 import BuscarPaciente from './buscarPaciente/BuscarPaciente';
 import reportWebVitals from './reportWebVitals';
 import * as Realm from "realm-web";
